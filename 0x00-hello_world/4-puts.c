774552ac4e5e850b46e21e4bec9b0c0c36f8c579
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-10 03:49:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: zero
+*
+*/
+int main(void)
+{	
+	char firsString[] = "\"Programming is like building a multilingual puzzle";
+	puts(firstString);
+	return(0);
+}
+
