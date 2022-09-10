8ce28e5219427757c16567fb3deab10e98d8f91b
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-10 05:39:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* This program prints "Programmin is like a building a multilingual puzzle"
-* Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * This program prints "Programming is like a multilingual puzzle"
+ * Return: Always 0 to show the program ran successfully
 */
-int main(void) 
-{	
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
