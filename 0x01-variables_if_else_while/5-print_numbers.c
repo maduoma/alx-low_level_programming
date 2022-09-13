b62563f33cae18285bc863fbe8c65eff323728af
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-12 21:33:04 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints 0-10
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
