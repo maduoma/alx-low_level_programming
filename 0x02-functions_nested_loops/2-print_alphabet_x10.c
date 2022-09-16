c672d1a885b85cc8203724cc516784e47e8d8f36
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-15 22:40:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase
+ * and adds a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
