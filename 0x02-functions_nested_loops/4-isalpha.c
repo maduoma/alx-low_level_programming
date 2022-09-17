c211f6207f1031bdc0bb576dd437b7c713d12600
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-17 03:48:57 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns 1 if c is a letter, lowercase or uppercase or 0 otherwise
+ * @c: The checked value
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
