ed69aab2cdd639b1b71df127cc8c9a6c1f3cf7a3
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-17 06:27:22 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: Second number
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum)
+}
