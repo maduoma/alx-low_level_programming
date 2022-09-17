4f6dd6d5cb0473d0649f4385383d39826b625992
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-17 03:02:08 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - Returns 1 if it is lower 
+ * _is:wer - Returns 1 if is is lower
  * or 0 otherwise
  * Return: Always 0
  */
 int _islower(int c)
 {
-	if (c >= 97 && C <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
