5e9d9dfdb367fb7bb2e295b47444569250097310
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-17 05:17:53 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Prints every minute of jack bauer.
+ * @hr: Responsible for hour
+ * @ms: Responsible for minute
+ * Return: Alwways 0
+ */
+void jack_bauer(void)
+{
+	int hr;
+	int ms;
+
+	for (hr = 0; hr <= 23; hr++)
+	
+	for (ms = 0; ms <= 59; ms++)
+	{
+		_putchar('0' + hr / 10);
+		_putchar('0' + hr % 10);
+		_putchar(':');
+		_putchar('0' + ms / 10);
+		_putchar('0' + ms % 10);
+		_putchar('\n');
+	}
+}
