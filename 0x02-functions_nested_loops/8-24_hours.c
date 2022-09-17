75aa69583681fe2b7fa544a2fdd949b446901d57
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-17 05:44:11 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 	{
 		for (ms = 0; ms <= 59; ms++)
 		{
-			_putchar('0' + hr / 10);
-			_putchar('0' + hr % 10);
+			_putchar('0' + (hr / 10));
+			_putchar('0' + (hr % 10));
 			_putchar(':');
-			_putchar('0' + ms / 10);
-			_putchar('0' + ms % 10);
+			_putchar('0' + (ms / 10));
+			_putchar('0' + (ms % 10));
 			_putchar('\n');
 		}
 	{
