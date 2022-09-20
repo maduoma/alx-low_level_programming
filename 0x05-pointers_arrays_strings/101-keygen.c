9a10182be8c480337bcdc8832bc52a69db7ad9bf
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-20 01:46:58 -0700

    Advanced

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - entry point of the c program
+ * Return: Always 0 in this program
+ */
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
