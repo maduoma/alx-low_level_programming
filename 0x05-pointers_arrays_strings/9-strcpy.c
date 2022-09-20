d8b7b7377da6744b9927d0fa09b8b3ed631bbcd5
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-19 22:08:42 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (src[i])
+	while (*(src + inc) != '\0')
 	{
-		dest[i] = src[i];
+		*(dest + inc) = *(src + i);
 		i++;
 	}
+	*(dest + i) = '\0';
+
 	return (dest);
 }
