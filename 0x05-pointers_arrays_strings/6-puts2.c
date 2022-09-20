3c10e7b69887bbee42c8e2296844091bc47331e6
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-19 20:11:46 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characte
+ * @str: character
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < string_lenght(str); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * string_length - finds the length of a string.
+ * @pointer: pointer.
+ * Return: length of c
+ */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
