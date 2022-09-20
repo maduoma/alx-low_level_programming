c1fd39219f1d6ad31da45c305eb8e48db5698191
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-19 19:49:00 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,34 @@
 
 /**
  * print_rev - Prints the string reverse
- * @str: This will hold the string
+ * @s: This will hold the string
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
+	int i;
 
-	if (str)
+	i = string_length(s) - 1;
+
+	while (i >= 0)
 	{
-		while (*str)
-		{
-			_putchar(*str++);
-		}
-	{
+		_putchar(s[i]);
+		i--;
+	}
 	_putchar('\n');
+}
+
+/**
+ * string_length - finds the length of a string
+ * @pointer: Pointer
+ * Return: Returns the length
+ */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while(*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
