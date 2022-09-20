71c9091ffc26deb5fa5e0a3bda4d67d6b36a156a
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-19 20:44:16 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * push_half - Prints half of a string
+ * @str: Character
+ */
+void push_half(char *str)
+{
+	int m, n, i, j;
+
+	m = string_length(str) / 2;
+
+	if (string_length(str) % 2 == 0)
+	{
+		for (i = m; i < string_lenth(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (string_length(str) % 2 != 0)
+	{
+		n = (string_lenth(str) - 1) / 2;
+		for (j = n; j <= string_length(str); j++)
+		{
+			_putchar(str[j]);
+		}
+        }
+	_putchar('\n');
+}
+
+
+/**
+ * string_length - finds the length of a string
+ * @pointer: pointer
+ * Return: length of c
+ */
+int string_lenth(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (0);
+}
