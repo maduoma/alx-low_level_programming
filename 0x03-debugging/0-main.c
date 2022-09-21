fda08c11f4d86a5cdfd59b20d68e4f447d8b9d13
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-21 10:37:50 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,16 @@
 /**
  * main -Function that prints if int is positive or negative
  * Return: Always 0
+ * @i: integer
  */
+
+void positive_or_negative(int i);
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
