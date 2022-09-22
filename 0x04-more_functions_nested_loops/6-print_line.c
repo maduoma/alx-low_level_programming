60cfd5eee7d8429f78790171e31fd745e5b67ba7
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-21 22:12:42 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - checks code
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
