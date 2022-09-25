bffc63eeb994d61256b41fc823ba829394196d7e
Maduabughichi Achilefu <18477889+maduoma@users.noreply.github.com>
2022-09-24 21:53:06 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: String to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string (dest)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int m = 0;
+	int k = 0;
+
+	while (dest[m] != '\0')
+		m++;
+
+	while (src[k] != src[n])
+	{
+		dest[m] = src[k];
+		m++;
+		k++;
+	}
+	dest[m] = '\0';
+
+	return (dest);
+}
