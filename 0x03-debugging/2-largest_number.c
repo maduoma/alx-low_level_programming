33d54cdae563d31c1ef2ae86ef287359797042ec
MADUABUGHICHI ACHILEFU <18477889+maduoma@users.noreply.github.com>
2022-10-03 06:10:10 +0100

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: First number
- * @b: Second integer
- * @c: Third integer
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
@@ -12,17 +12,20 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
+
 	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
